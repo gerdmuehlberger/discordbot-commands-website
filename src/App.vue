f0fb96db35f8c9b610383aaedae0fb69e9b351b4
zoidgerd <gerd.muehlberger96@gmail.com>
2021-02-13 23:35:53 +0100

    setup site done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <h3>bot prefix: "pahie"</h3>
+  <button class="modalbutton" v-for='command in commands.commands' :key="command.name" @click="sendDataToModal(command.name, command.desc, command.args); toggleCommandModal();"> 
+    {{command.name}}
+  </button>
+
+  <teleport to=".modals" v-if="showCommandModal">
+    <Modal :modalheader="selectedCommand" :modaldesc="selectedCommandDescription" :modalargs="selectedCommandArgs" :darkmode="darkMode" @close="toggleCommandModal"></Modal>    
+  </teleport>
+
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+import commands from './bot-commands.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  
+  data() {
+    return {
+      title: 'Pahiebot Commands:',
+      commands: JSON.parse(JSON.stringify(commands)),
+      selectedCommand: '',
+      selectedCommandDescription:'',
+      selectedCommandArgs: '',
+      showCommandModal: false,
+      darkMode: false
+    }
+  },
+  methods: {
+    toggleDarkMode(){
+      this.darkMode = !this.darkMode
+      console.log()
+    },
+    toggleCommandModal(){
+      this.showCommandModal = !this.showCommandModal
+    },
+    sendDataToModal(commandname, commanddesc, commandargs) {
+      this.selectedCommand = commandname;
+      this.selectedCommandDescription = commanddesc;
+      this.selectedCommandArgs = commandargs;
+    }
   }
 }
 </script>
@@ -20,7 +58,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+}
+
+h1{
+  font-size: 5em;
+  margin-bottom: 0px;
+  color: #ffffff;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="back" @click.self="closeCommandModal">
+        <div class="modal" :class="{ darkmode: darkmode === true }">
+            <h1>{{ modalheader }}</h1>
+            <p id="p-desc">{{ modaldesc }}</p>
+            <p id="p-args">Arguments: {{ modalargs }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['modalheader', 'modaldesc', 'modalargs', 'darkmode'],
+    methods: {
+        closeCommandModal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+
+        width: 450px;
+        height: 200px;
+        padding: 10px;
+        margin: 15% auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .back {
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    h1{
+        margin-top: 10%;
+        color:#7289da;
+        font-size: 2em;
+    }
+
+
+    #p-desc{
+        font-style: italic;
+    }
+
+
+    #p-args{
+        font-weight: bold;
+    }
+
+    .darkmode{
+        background: black;
+        color: white;
+
+    }
+
+    .darkmode h1{
+        color: white;
+    }
+
+
+</style>
